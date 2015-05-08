b2f8886937ac3672cb275976bd4c14406d44e73a
Jennifer <yortree@gmail.com>
2015-05-08 11:00:58 -0700

    Update app.rb with database method.

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/task')
+require('pg')
+
+DB. = PG.connect({:dbname => 'salon_system'})
 
 get('/') do
   @stylist = Stylist.all()
@@ -10,7 +13,7 @@ end
 
 post('/stylists') do
   name = params.fetch("name")
-  stylist = Stylist.new(name)
+  stylist = Stylist.new({:name => name})
   stylist.save()
   erb(:success)
 end
