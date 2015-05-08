abb9d4401d92072433e04593bec36468a84ed20f
Jennifer <yortree@gmail.com>
2015-05-08 10:58:17 -0700

    Update stylist_spec with database method.

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,37 +1,28 @@
 require("rspec")
-require("Stylist")
+require("pg")
+require("stylist")
+
+DB = PG.connect({:dbname => "salon_system_test"})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM stylsts *;")
+  end
+end
 
 describe(Stylist) do
-  before do
-    Stylist.clear()
-  end
-  
-    describe("#name") do
-      it("gives you a name of a stylist") do
-      test_stylist = Stylist.new("Monica")
-      expect(test_stylist.name()).to(eq("Monica"))
-    end
-  end
 
   describe(".all")do
     it("is empty at first") do
-      expect(Stylist.all()).to(eq([]))
+    expect(Stylist.all()).to(eq([]))
     end
   end
 
-  describe("#save") do
-    it("adds a stylist to the array of saved stylists") do
-      test_stylist = Stylist.new("Evelyn")
-      test_stylist.save()
-      expect(Stylist.all()).to(eq([test_stylist]))
-    end
-  end
 
-  describe(".clear") do
-    it("empties out all of the saved stylists") do
-      Stylist.new("Monica").save()
-      Stylist.clear()
-      expect(Stylist.all()).to(eq([]))
+  describe("#name") do
+    it("gives you a name of a stylist") do
+      test_stylist = Stylist.new("Monica")
+      expect(test_stylist.name()).to(eq("Monica"))
     end
   end
 end
